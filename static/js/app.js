db288a80408717a62f17764ff19a6b13c211d1e8
Stefanía B. Ólafsdóttir <stefania.bjarney@gmail.com>
2014-07-08 02:03:47 +0000

    Restructure: Now using GPS vectors from "you" to "places", and not .png of map.

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,51 @@
+var windowWidth=window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+var windowHeight=window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+
+// Define the screen center
+var center = {
+    left: windowWidth / 2.0,
+    top: windowHeight / 2.0
+};
+
+var crossbow = {
+    id: "crossbow",
+    icon_path: '/static/img/x.png',
+    position: {latitude: 66.152076, longitude: -18.908227}
+};
+var skull = {
+    id: "skull",
+    icon_path: '/static/img/skull.svg',
+    position: {latitude: 66.148903, longitude: -18.905641}
+};
+var places = [crossbow, skull];
+
+var scale = 20000.0;
+
 
 $(function () {
+    // for (var place in places) {
+
+    // }
+
+    $("#viewport").height(windowHeight);
+
+    // If we access the user's geolocation:
     if (navigator.geolocation) {
+        // Follow position changes as events
         navigator.geolocation.watchPosition(positionChanged);
     }
     else {
         alert("No GPS!");
     }
 
+    // If the user's device has orientation (LG7II does not have it for example)
     if (window.DeviceOrientationEvent) {
+        // Follow the orientation changes as events
         window.addEventListener('deviceorientation', deviceOrientationChanged, false);
     }
     else {
@@ -18,87 +56,45 @@ $(function () {
 
 
 function positionChanged(position) {
+
     var coords = position.coords;
 
-    var windowWidth=window.innerWidth
-    || document.documentElement.clientWidth
-    || document.body.clientWidth;
-
-    var windowHeight=window.innerHeight
-    || document.documentElement.clientHeight
-    || document.body.clientHeight;
-
-    // TODO: Doesn't make sense to be here, move into initalization
-    $("#viewport").height(windowHeight);
-    $("#you").css("left", windowWidth / 2.0 - $("#you").width() / 2.0);
-    $("#you").css("top", windowHeight / 2.0 - $("#you").height() / 2.0);
-
-    var imgWidth = 2880;
-    var imgHeight = 1444;
-
-    // coords = {
-    //     latitude:  66.1529,
-    //     longitude: -18.907,
-    //     accuracy: 1
-    // }
-
-    var gpsFrame = {
-        latitude: {min: 66.146800, max: 66.159258},
-        longitude: {min: -18.937770, max: -18.876316}
+    var you = {
+        id: "you",
+        icon_path: '/static/img/you.svg',
+        position: {latitude: coords.latitude, longitude: coords.longitude}
     };
 
-    var gpsRange = {
-        latitude: gpsFrame.latitude.max - gpsFrame.latitude.min,
-        longitude: gpsFrame.longitude.max - gpsFrame.longitude.min
-    };
+    // Set map as parent around the places
+    $("#map").css("position", "relative");
+    // Locate upper right corner of map to center of screen
+    $("#map").css("left", center.left);
+    $("#map").css("top", center.top);
 
-    var pxlPerDegree = {
-        x: imgWidth / gpsRange.longitude,
-        y: -1 * imgHeight / gpsRange.latitude
-    };
+    // Add you to screen, set position relative to map (it's parent)
+    $("#you").css("position", "absolute").css("width", "20px").css("z-index", 100);
+    // Set center of "you" circle to upper right corner of map
+    $("#you").css("left", - $("#you").width() / 2.0);
+    $("#you").css("top", - $("#you").height() / 2.0);
 
-    var degreesFrom = {
-        left: (gpsFrame.longitude.min - coords.longitude),
-        top: (gpsFrame.latitude.max - coords.latitude)
-    };
+    // Add places to map
+    $("#crossbow").css("position", "absolute").css("width", "20px");
+    $("#skull").css("position", "absolute").css("width", "20px");
 
-    var pixelsFrom = {
-        left: degreesFrom.left * pxlPerDegree.x,
-        top: degreesFrom.top * pxlPerDegree.y
-    };
-
-    var youPosition = {
-        left: windowWidth / 2,
-        top: windowHeight / 2
-    };
-
-    $("#map").css("left", youPosition.left + pixelsFrom.left);
-    $("#map").css("top", youPosition.top + pixelsFrom.top);
-    $("#map").css("transform-origin",  "" + (-pixelsFrom.left) + "px "
-                                          + (-pixelsFrom.top) + "px");
+    // Locate places relative to you, and scale map
+    $("#crossbow").css("left", (crossbow.position.longitude - you.position.longitude) * scale);
+    $("#crossbow").css("top", -(crossbow.position.latitude - you.position.latitude) * scale);
+    $("#skull").css("left", (skull.position.longitude - you.position.longitude) * scale);
+    $("#skull").css("top", -(skull.position.latitude - you.position.latitude) * scale);
 
 
-//     $("#debug #location").text("Timestamp: " + position.timestamp
-//         + ", lat: " + coords.latitude
-//         + ", lon: " + coords.longitude
-//         + ", accuracy: " + coords.accuracy
-//     );
+    $("#debug #location").text("Timestamp: " + position.timestamp
+        + ", lat: " + coords.latitude
+        + ", lon: " + coords.longitude
+        + ", accuracy: " + coords.accuracy
+    );
 //     $("#debug #window").text("Window width: " + window.innerWidth
 //         + ", window height: " + window.innerHeight
-//     );
-
-//     $("#debug #gpsframe").text("Latitude range: " + gpsFrame.latitude.min + ' - ' + gpsFrame.latitude.max
-//         + "Longitude range: " + gpsFrame.longitude.min + ' - ' + gpsFrame.longitude.max
-//         + " RANGE: " + gpsRange.longitude + ' and ' + gpsRange.latitude
-//     );
-
-//     $("#debug #convertion").text(
-//         'Pixels per degree: longitude' + pxlPerDegree.y + ' latitude: ' + pxlPerDegree.x
-//     );
-
-//     $("#debug #framelocation").text(
-//         'Degrees from top: ' + degreesFrom.top + ' Degrees from left: ' + degreesFrom.left
-//         + 'Pixels from top: ' + pixelsFrom.top + ' Pixels from left: ' + pixelsFrom.left
 //     );
 }
 
@@ -110,12 +106,12 @@ function deviceOrientationChanged(orientationEvent)
 
     if(compassDirection)
     {
-        $("#debug #orientation").text(
-            "Left Right: " + tiltLeftRight +
-            " Front Back: " + tiltFrontBack +
-            " Compass Direction: " + compassDirection
-        );
+        // $("#debug #orientation").text("Degrees from North: " + compassDirection);
 
-        $("#map").css("transform", "rotate(" + (- compassDirection) + "deg)");
+
+        // Set z-axis of rotation at 0% of map, i.e. center of screen
+        $("#map").css("transform-origin",  "" + (0) + "% " + (0) + "%");
+        // Rotate map around "you"
+        $("#map").css("transform", "rotate(" + (-compassDirection) + "deg)");
     }
 }
